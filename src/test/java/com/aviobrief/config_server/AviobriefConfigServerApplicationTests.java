149f19a15e28bc5fbc2c1cbbb1eec61d9cc015bb
Petar-I-Petkov <cpt.petkov@gmail.com>
2021-05-09 06:39:57 +0300

    Init commit src & pom.xml

---
+++ src/test/java/com/aviobrief/config_server/AviobriefConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviobrief.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviobriefConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
